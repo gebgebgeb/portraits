7affc66a93fc3b58058c78433f1e69b5a4270c70
dmahfouda <david.mahfouda@gmail.com>
2018-10-15 15:48:22 -0400

    added ids

--- drawer.js
+++ drawer.js
@@ -1,7 +1,9 @@
-var peer = new Peer({key: 'lwjd5qra8257b9'});
+var peer = new Peer({key: 'lwjd5qra8257b9', id:'a'});
+
 navigator.mediaDevices.getUserMedia({video:true, audio:false})
 .then(function(mediaStream) {
 	// Call a peer, providing our mediaStream
-	let call = peer.call('dest-peer-id',
+	let call = peer.call('b',
 		mediaStream);
+	console.log(peer.id)
 })
--- sitter.js
+++ sitter.js
@@ -1,5 +1,7 @@
-var peer = new Peer({key: 'lwjd5qra8257b9'});
+var peer = new Peer('b', {key: 'lwjd5qra8257b9'});
+
 peer.on('call', function(call) {
 	// Answer the call, providing our mediaStream
 	call.answer();
+	console.log("received")
 });
