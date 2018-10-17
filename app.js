7f4b19de70409456dc85f6fe960a04f21efaf7e5
dmahfouda <david.mahfouda@gmail.com>
2018-10-17 15:27:33 -0400

    added express server

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var port = 3000;
+
+var app = express();
+
+app.use(express.static('build'))
+
+app.get("/", (req, res) => {
+ res.sendFile(__dirname + "/build/index.html");
+});
+
+app.listen(port, () => {
+	console.log("Server listening on port "+port)
+})
