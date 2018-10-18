8d32dcb39939ceca846238c6e7937ba3356a7d9a
gebgebgeb <asdf>
2018-10-18 14:59:40 -0400

    adding key back in to peerjs server

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var port = process.env.PORT || 9000;
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 var app = express();
 var options = {
-    debug: true
+  debug: true
+	, key:'peerjs'
 }
 
 app.use(express.static('build'))
