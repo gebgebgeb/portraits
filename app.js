6a967baabd03f9743098cbcc2b4d44b7b09a8c1f
dmahfouda <david.mahfouda@gmail.com>
2018-10-18 15:21:06 -0400

    hard coded port 80

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var port = 9000;
+var port = process.env.PORT || 80;
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 var app = express();
 var options = {
