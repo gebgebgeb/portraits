e90cc4f6a0cc0624e3a82ec7f8c748bed8f62f33
gebgebgeb <asdf>
2018-10-18 15:13:06 -0400

    dynamically getting peerjs port

--- build/drawer.js
+++ build/drawer.js
@@ -1,10 +1,11 @@
 let urlParams = new URLSearchParams(window.location.search);
 let sitterId = urlParams.get('sitterId')
 let drawerId = urlParams.get('drawerId')
+let port = urlParams.get('port')
 
 console.log(drawerId)
 
-const peer = new Peer(drawerId, {key:'peerjs', port:9000, host:'sleepy-earth-42956.herokuapp.com', path: '/api', debug:3});
+const peer = new Peer(drawerId, {key:'peerjs', port:port, host:'sleepy-earth-42956.herokuapp.com', path: '/api', debug:3});
 
 peer.on('open', function(id) {
   console.log('My peer ID is: ' + id);
--- build/index.js
+++ build/index.js
@@ -1,8 +1,11 @@
+let urlParams = new URLSearchParams(window.location.search);
+let port = urlParams.get('port');
+
 let sitterLink = document.getElementById("sitterLink")
 let drawerLink = document.getElementById("drawerLink")
 
 let randomPeerDrawer = Math.floor((Math.random()*1000))
 let randomPeerSitter = Math.floor((Math.random()*1000))
 
-sitterLink.href = "sitter.html?sitterId="+randomPeerSitter+"&drawerId="+randomPeerDrawer
-drawerLink.href = "drawer.html?drawerId="+randomPeerDrawer+"&sitterId="+randomPeerSitter
+sitterLink.href = "sitter.html?sitterId="+randomPeerSitter+"&drawerId="+randomPeerDrawer+'&port='+port
+drawerLink.href = "drawer.html?drawerId="+randomPeerDrawer+"&sitterId="+randomPeerSitter+'&port='+port
