9f0f2d3296f10e1f8ff85edf09977fcd1037aadf
dmahfouda <david.mahfouda@gmail.com>
2018-10-29 16:18:10 -0400

    production & dev url variables

--- build/drawer.js
+++ build/drawer.js
@@ -6,13 +6,21 @@ let urlParams = new URLSearchParams(window.location.search)
 let sitterId = urlParams.get('sitterId')
 let drawerId = urlParams.get('drawerId')
 
+
+let productionServer = window.location.hostname.indexOf("localhost") === -1
+
 let scaleFactor
 let videoWidth
 let videoHeight
 
-const peer = new Peer(drawerId, {key:'peerjs', port:443, host:'sleepy-earth-42956.herokuapp.com', path: '/api', debug:3})
-//const peer = new Peer(drawerId, {key:'peerjs', port:9000, host:'localhost', path: '/api', debug:3})
-// const peer2 = new Peer(drawerId, {key:'peerjs', port:9000, host:'localhost', path: '/api', debug:3})
+let peer
+
+if (productionServer) {
+	peer = new Peer(drawerId, {key:'peerjs', port:443, host:'sleepy-earth-42956.herokuapp.com', path: '/api', debug:3})
+} else {
+	peer = new Peer(drawerId, {key:'peerjs', port:9000, host:'localhost', path: '/api', debug:3})
+
+}
 
 peer.on('open', function(id) {
   console.log('My peer ID is: ' + id)
--- build/sitter.js
+++ build/sitter.js
@@ -4,8 +4,16 @@ const videoOfDrawerCanvas = document.getElementById('videoOfDrawerCanvas')
 let urlParams = new URLSearchParams(window.location.search);
 let sitterId = urlParams.get('sitterId');
 
-const peer = new Peer(sitterId, {key: 'peerjs', port:443, host:'sleepy-earth-42956.herokuapp.com', path: '/api', debug:3});
-//const peer = new Peer(sitterId, {key: 'peerjs', port:9000, host:'localhost', path: '/api', debug:3});
+let productionServer = window.location.hostname.indexOf("localhost") === -1
+
+let peer
+
+if (productionServer) {
+	peer = new Peer(sitterId, {key:'peerjs', port:443, host:'sleepy-earth-42956.herokuapp.com', path: '/api', debug:3})
+} else {
+	peer = new Peer(sitterId, {key:'peerjs', port:9000, host:'localhost', path: '/api', debug:3})
+
+}
 
 peer.on('open', function(id) {
   console.log('My peer ID is: ' + id);
