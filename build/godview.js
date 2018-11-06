16643d2883a9d7bc715665ce7eb1f5792ec90a57
gebgebgeb <asdf>
2018-11-06 15:40:25 -0500

    view replays on godview

--- build/godview.js
+++ build/godview.js
@@ -7,9 +7,42 @@ let productionServer = window.location.hostname.indexOf("localhost") === -1
 
 let peer
 
+const draw = (canvas, mousePositionArray, mousePosIdx) => {
+	let ctx = canvas.getContext('2d')
+	let lastMousePos = mousePositionArray[mousePosIdx-1]
+	let mousePos = mousePositionArray[mousePosIdx]
+	console.log(mousePos.mouseUp)
+	if(!lastMousePos.mouseUp){
+		ctx.beginPath()
+		ctx.moveTo(lastMousePos.x, lastMousePos.y)
+		ctx.lineTo(mousePos.x, mousePos.y)
+		ctx.lineWidth = 1
+		ctx.strokeStyle = '#009900'
+		ctx.stroke()
+	}
+	if(mousePosIdx < mousePositionArray.length - 1){
+		setTimeout(()=>draw(canvas, mousePositionArray, mousePosIdx + 1), 150)
+	}else{
+		setTimeout(()=>{
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			draw(canvas, mousePositionArray, 1)
+		}, 500)
+	}
+}
 
 axios.get('/previousportraits').then((response)=>{
 	console.log(response)
+	for(pastPortrait of response.data){
+		videos.push(document.createElement("canvas"))
+		col = document.createElement('div')
+		col.className = 'col-4'
+		videos[videos.length-1].width = pastPortrait.canvasWidth
+		videos[videos.length-1].height = pastPortrait.canvasHeight
+		videos[videos.length-1].className = 'godViewCanvas'
+		col.appendChild(videos[videos.length-1])
+		row.appendChild(col)
+		draw(videos[videos.length-1], pastPortrait.mousePositionArray, 1)
+	}
 })
 
 // 	let godViewId = response.data.godViewId
