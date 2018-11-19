39c99077b631c6661d92e6a105153672429c65ed
dmahfouda <david.mahfouda@gmail.com>
2018-11-19 15:17:50 -0500

    added substantive mailto link from previous portrait

--- build/previousPortrait.js
+++ build/previousPortrait.js
@@ -1,7 +1,9 @@
 let urlParams = new URLSearchParams(window.location.search)
 let portraitId = urlParams.get('id')
-
+let link = document.getElementById('linkToPreviousPortrait')
 let previousCanvas = document.getElementById('previousCanvas')
+
+link.href = 'mailto:?subject=Check+Out+This+Portrait+I+Made&body=I+made+a+portrait,+you+can+see+it+here:+'+window.location
 
 axios.get('/previousportrait', {params:{_id: portraitId}}).then((response)=>{
 	previousCanvas.width = response.data.canvasWidth
